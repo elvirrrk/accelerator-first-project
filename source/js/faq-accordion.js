797e0d5a2a8677d6e703fd82b3fbaa4765979ba4
Elvira Fatin <elvirrrk@gmail.com>
2024-04-02 01:13:48 +0300

    start contact&form block

--- source/js/faq-accordion.js
+++ source/js/faq-accordion.js
@@ -2,17 +2,40 @@ const faqWrapper = document.querySelector('.faq__wrapper');
 const liAccordion = faqWrapper.querySelectorAll('li');
 const firstLi = faqWrapper.querySelector('li');
 
-firstLi.querySelector('p').style.display = 'block';
+firstLi.querySelector('p').style.position = 'static';
+firstLi.querySelector('p').style.opacity = '1';
 firstLi.querySelector('input').checked = 'true';
 
-liAccordion.forEach((item) => {
-  item.addEventListener('click', () => {
-    const inputAccodion = item.querySelector('input[type="checkbox"]');
+liAccordion.forEach((liElement) => {
+  liElement.addEventListener('click', () => {
+    const inputAccodionClick = liElement.querySelector('input[type="checkbox"]');
+    openAccordion(liElement, inputAccodionClick);
+  });
+});
 
-    if (inputAccodion.checked) {
-      item.querySelector('p').style.display = 'block';
-    } else {
-      item.querySelector('p').style.display = 'none';
+liAccordion.forEach((liElement) => {
+  liElement.addEventListener('keydown', (evt) => {
+    if(evt.key === 'Enter') {
+      const inputAccodionEnter = liElement.querySelector('input[type="checkbox"]');
+      if(inputAccodionEnter.checked) {
+        inputAccodionEnter.checked = false;
+        liElement.querySelector('p').style.opacity = '0';
+        liElement.querySelector('p').style.position = 'absolute';
+      } else {
+        inputAccodionEnter.checked = 'checked';
+        liElement.querySelector('p').style.opacity = '1';
+        liElement.querySelector('p').style.position = 'static';
+      }
     }
   });
 });
+
+function openAccordion(item, inputAccodion) {
+  if (inputAccodion.checked) {
+    item.querySelector('p').style.opacity = '1';
+    item.querySelector('p').style.position = 'static';
+  } else {
+    item.querySelector('p').style.opacity = '0';
+    item.querySelector('p').style.position = 'absolute';
+  }
+}
